7e4eec1b0f3c79c866dcf0de60baffb85b34dc93
adkaras161 <adrian.karas161@gmail.com>
2024-07-09 00:49:19 +0200

    added home page v0.2 | login page v0.1

--- login/views.py
+++ login/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello_world_fun(request):
-    return render(request, 'hello.html')
+def log_fun(request):
+    return render(request, 'login.html')
